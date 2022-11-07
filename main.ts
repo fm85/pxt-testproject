812f6d02fc3db4955d9c73c77044f93095c3c1c8
fm85 <47183840+fm85@users.noreply.github.com>
2022-11-07 11:24:43 +0100

    first example

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-
+loops.everyInterval(1000, function () {
+    if (IoTCube.checkEvent(eRAK_EVT.JOINED)) {
+        music.playTone(523, music.beat(BeatFraction.Eighth))
+        basic.showIcon(IconNames.Yes)
+    } else if (IoTCube.checkEvent(eRAK_EVT.JOIN_FAILED)) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Diamond)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallDiamond)
+    }
+})
+loops.everyInterval(60000, function () {
+    if (IoTCube.checkEvent(eRAK_EVT.JOINED)) {
+        IoTCube.addAnalogInput(pins.analogReadPin(AnalogPin.P1), Channels.One)
+        IoTCube.SendBuffer(IoTCube.getCayenne(), Channels.One)
+    }
+})
+basic.forever(function () {
+	
+})
